ccfa87e7ce6ccee122347ed534e9622866290428
Kameron Decker Harris <kamdh@uw.edu>
2016-08-28 01:06:03 -0700

    removed matlab files

--- plotting/bounding_box.m
+++
@@ -1,3 +0,0 @@
-function [lows,highs]=bounding_box(vox)
-    lows=min(vox);
-    highs=max(vox);
--- plotting/index_lookup_map.m
+++
@@ -1,6 +0,0 @@
-function Y=index_lookup_map(X)
-    Y=containers.Map();
-    for i=1:length(X)
-        s=num2str(X(i,:));
-        Y(s)=i;
-    end
